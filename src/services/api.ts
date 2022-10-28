defb77ba488c2f23828f10fd6d739a0d24d737cb
Lucas Matheus Vieira Lúcio <lucmvl2003@gmail.com>
2022-10-28 13:29:31 -0300

    production-change

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,10 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    //In development ambient
+    // baseURL: 'http://localhost:3000/api',
+    
+    //In production ambient
+    baseURL: 'https://dtmoney-caster.netlify.app//api',
+
 })
